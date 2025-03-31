f67daa417089edcf7143e28291b4337e23fc4d69
Marc Munene <marcmunene104@gmail.com>
2025-04-01 00:24:59 +0300

    methods

--- Server/src/server.js
+++ Server/src/server.js
@@ -4,6 +4,41 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+//Home Route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Home Route",
+  });
+});
+
+//Get blogs
+app.get("/blogs", (req, res) => {
+  res.json({
+    message: "Getting Blogs ",
+  });
+});
+
+//post blogs
+app.post("/blogs", (req, res) => {
+  res.json({
+    message: "Creating Blogs",
+  });
+});
+
+//put blogs
+app.put("/blogs", (req, res) => {
+  res.json({
+    message: "Updating Blogs",
+  });
+});
+
+//delete blogs
+app.delete("/blogs", (req, res) => {
+  res.json({
+    message: "Deleting Blogs",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`working Successfully at port no ${PORT}`);
 });
