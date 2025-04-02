2df825fd8b315c6d342bbd8cdde1dd957fd34a1f
Marc Munene <marcmunene104@gmail.com>
2025-04-02 21:09:29 +0300

    get users

---
+++ Server/src/controllers/users.js
@@ -0,0 +1,19 @@
+import { User } from "../database/models/user.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const user = await User.find();
+
+    res.status(200).json({
+      success: true,
+      message: "User found!",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to get users",
+    });
+  }
+};
